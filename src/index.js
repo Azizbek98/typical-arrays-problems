9f80726e372e6677f17dcb0f6346bd6859916368
Azizbek98 <azizbeksamatov98@gmail.com>
2021-07-07 22:48:46 +0500

    feat: updating some changes

---
+++ src/index.js
@@ -0,0 +1,35 @@
+
+exports.min = function min (array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+    if (array.length > 0) {
+      return Math.min(...array); // Using ... destructor of array
+    } else {
+      return 0;
+    }
+}
+
+exports.max = function max (array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+    if (array.length > 0) {
+      return Math.max(...array); // Using ... destructor of array
+    } else {
+      return 0;
+    }
+}
+
+exports.avg = function avg (array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += parseInt(array[i], 10);
+  }
+  let avg = sum / array.length;
+  
+  return avg;
+}
